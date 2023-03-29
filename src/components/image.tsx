575524b1e3d448c8b38a0897f2631042233a926b
doyoungjeong <arktics@naver.com>
2023-03-29 09:55:40 +0900

    이미지 컴포넌트 예외처리

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,12 +1,24 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
-const Img = (Props: any) => {
+type ImageState = "loading" | "load" | "error";
+
+const Img = React.memo((Props: any) => {
+  const [state, setState] = useState<ImageState>("loading");
   useLayoutEffect(() => {
     const image = new Image();
     image.src = Props.src;
+    image.onload = (e) => {
+      setState("load");
+    }
+    image.onerror = (e) => {
+      setState("error");
+    }
   }, []);
 
-  return <img {...Props} />;
-};
+  if (state === "loading") return <div className='imagebox'>Loading...</div>
+  if (state === "error") return <div className='imagebox'>이미지를 불러올 수 없습니다.</div>
+
+  return <img {...Props}  />;
+});
 
 export default Img;
